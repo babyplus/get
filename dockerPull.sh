2eb871b6ab35113f340ebedb60de00e9ee697475
babyplus <maxcrazy@yeah.net>
2019-11-07 00:25:53 +0800

    release-vc3c03.kube_controller_manager.v1_15_1 k8s.gcr.io/kube-controller-manager:v1.15.1

---
+++ dockerPull.sh
@@ -0,0 +1,1 @@
+docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:aa696.kube_apiserver.v1_15_1
---
+++ dockerTag.sh
@@ -0,0 +1,1 @@
+docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:aa696.kube_apiserver.v1_15_1 k8s.gcr.io/kube-apiserver:v1.15.1
