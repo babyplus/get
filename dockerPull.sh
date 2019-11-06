e81d8a612791fa7aa8b3e9cfc049fb45305623e6
babyplus <maxcrazy@yeah.net>
2019-11-07 00:27:11 +0800

    release-v8dad4.kube_proxy.v1_15_1 k8s.gcr.io/kube-proxy:v1.15.1

--- dockerPull.sh
+++ dockerPull.sh
@@ -1,2 +1,3 @@
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:aa696.kube_apiserver.v1_15_1
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:c3c03.kube_controller_manager.v1_15_1
+docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:02ec0.kube_scheduler.v1_15_1
--- dockerTag.sh
+++ dockerTag.sh
@@ -1,2 +1,3 @@
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:aa696.kube_apiserver.v1_15_1 k8s.gcr.io/kube-apiserver:v1.15.1
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:c3c03.kube_controller_manager.v1_15_1 k8s.gcr.io/kube-controller-manager:v1.15.1
+docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:02ec0.kube_scheduler.v1_15_1 k8s.gcr.io/kube-scheduler:v1.15.1
