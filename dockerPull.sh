8102a1b85c855d78892da51dc5a133dfab4243b0
babyplus <maxcrazy@yeah.net>
2019-11-07 00:29:07 +0800

    release-v78447.coredns.1_3_1 k8s.gcr.io/coredns:1.3.1

--- dockerPull.sh
+++ dockerPull.sh
@@ -3,3 +3,4 @@ docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:c3c03.kube_controller
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:02ec0.kube_scheduler.v1_15_1
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:8dad4.kube_proxy.v1_15_1
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:84041.pause.3_1
+docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:72e7e.etcd.3_3_10
