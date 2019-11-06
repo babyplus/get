03639f0fdaf823330a71ea04f504eaea3d81adfd
babyplus <maxcrazy@yeah.net>
2019-11-06 23:18:32 +0800

    release-vfeedf.kube_scheduler.v1_15_1 k8s.gcr.io/kube-scheduler:v1.15.1

--- dockerPull.sh
+++ dockerPull.sh
@@ -1,1 +1,2 @@
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:92b66.kube_apiserver.v1_15_1
+docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:ac332.kube_controller_manager.v1_15_1
--- dockerTag.sh
+++ dockerTag.sh
@@ -1,1 +1,2 @@
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:92b66.kube_apiserver.v1_15_1 k8s.gcr.io/kube-apiserver:v1.15.1
+docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:ac332.kube_controller_manager.v1_15_1 k8s.gcr.io/kube-controller-manager:v1.15.1
