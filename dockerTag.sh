8146a279fde117a0d7f09cff3ad65bb1e2a22695
babyplus <maxcrazy@yeah.net>
2019-11-07 00:28:28 +0800

    release-v72e7e.etcd.3_3_10 k8s.gcr.io/etcd:3.3.10

--- dockerTag.sh
+++ dockerTag.sh
@@ -2,3 +2,4 @@ docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:aa696.kube_apiserver.v
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:c3c03.kube_controller_manager.v1_15_1 k8s.gcr.io/kube-controller-manager:v1.15.1
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:02ec0.kube_scheduler.v1_15_1 k8s.gcr.io/kube-scheduler:v1.15.1
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:8dad4.kube_proxy.v1_15_1 k8s.gcr.io/kube-proxy:v1.15.1
+docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:84041.pause.3_1 k8s.gcr.io/pause:3.1
