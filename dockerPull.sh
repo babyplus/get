686009ac1c14d6161ea4516779a22d0a2079497c
babyplus <maxcrazy@yeah.net>
2019-11-06 23:17:47 +0800

    release-vac332.kube_controller_manager.v1_15_1 k8s.gcr.io/kube-controller-manager:v1.15.1

---
+++ dockerPull.sh
@@ -0,0 +1,1 @@
+docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:92b66.kube_apiserver.v1_15_1
---
+++ dockerTag.sh
@@ -0,0 +1,1 @@
+docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:92b66.kube_apiserver.v1_15_1 k8s.gcr.io/kube-apiserver:v1.15.1
