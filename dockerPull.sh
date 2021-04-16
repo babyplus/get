751287a22d1f939d6d479473da9d3c695a4bb1dd
plus <maxcrazy@yeah.net>
2021-04-16 14:47:19 +0800

    release-v3d9c7.kube_controller_manager.v1_18_1 k8s.gcr.io/kube-controller-manager:v1.18.1

---
+++ dockerPull.sh
@@ -0,0 +1,1 @@
+docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:3fa66.kube_apiserver.v1_18_1
---
+++ dockerTag.sh
@@ -0,0 +1,1 @@
+docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:3fa66.kube_apiserver.v1_18_1 k8s.gcr.io/kube-apiserver:v1.18.1
