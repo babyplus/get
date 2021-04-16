1854448ce849c5990cac34b6551fa603e5cf7a36
plus <maxcrazy@yeah.net>
2021-04-16 14:51:13 +0800

    release-vade4d.coredns.1_6_7 k8s.gcr.io/coredns:1.6.7

--- dockerPull.sh
+++ dockerPull.sh
@@ -3,3 +3,4 @@ docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:3d9c7.kube_controller
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:96880.kube_scheduler.v1_18_1
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:cd2cc.kube_proxy.v1_18_1
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:dc140.pause.3_2
+docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:16dfb.etcd.3_4_3-0
--- dockerTag.sh
+++ dockerTag.sh
@@ -3,3 +3,4 @@ docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:3d9c7.kube_controller_
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:96880.kube_scheduler.v1_18_1 k8s.gcr.io/kube-scheduler:v1.18.1
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:cd2cc.kube_proxy.v1_18_1 k8s.gcr.io/kube-proxy:v1.18.1
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:dc140.pause.3_2 k8s.gcr.io/pause:3.2
+docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:16dfb.etcd.3_4_3-0 k8s.gcr.io/etcd:3.4.3-0
