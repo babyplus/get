722187107d22d072a2db3ae9e88b1bb126e4bfc9
plus <maxcrazy@yeah.net>
2021-04-16 14:46:43 +0800

    release-v3fa66.kube_apiserver.v1_18_1 k8s.gcr.io/kube-apiserver:v1.18.1

--- dockerTag.sh
+++
@@ -1,6 +0,0 @@
-docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:aa696.kube_apiserver.v1_15_1 k8s.gcr.io/kube-apiserver:v1.15.1
-docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:c3c03.kube_controller_manager.v1_15_1 k8s.gcr.io/kube-controller-manager:v1.15.1
-docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:02ec0.kube_scheduler.v1_15_1 k8s.gcr.io/kube-scheduler:v1.15.1
-docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:8dad4.kube_proxy.v1_15_1 k8s.gcr.io/kube-proxy:v1.15.1
-docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:84041.pause.3_1 k8s.gcr.io/pause:3.1
-docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:72e7e.etcd.3_3_10 k8s.gcr.io/etcd:3.3.10
