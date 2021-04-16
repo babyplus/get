e9ea80036744b281b77616945ba277bfc72725ff
plus <maxcrazy@yeah.net>
2021-04-16 14:50:06 +0800

    release-v16dfb.etcd.3_4_3-0 k8s.gcr.io/etcd:3.4.3-0

--- dockerPull.sh
+++ dockerPull.sh
@@ -2,3 +2,4 @@ docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:3fa66.kube_apiserver.
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:3d9c7.kube_controller_manager.v1_18_1
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:96880.kube_scheduler.v1_18_1
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:cd2cc.kube_proxy.v1_18_1
+docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:dc140.pause.3_2
--- dockerTag.sh
+++ dockerTag.sh
@@ -2,3 +2,4 @@ docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:3fa66.kube_apiserver.v
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:3d9c7.kube_controller_manager.v1_18_1 k8s.gcr.io/kube-controller-manager:v1.18.1
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:96880.kube_scheduler.v1_18_1 k8s.gcr.io/kube-scheduler:v1.18.1
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:cd2cc.kube_proxy.v1_18_1 k8s.gcr.io/kube-proxy:v1.18.1
+docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:dc140.pause.3_2 k8s.gcr.io/pause:3.2
