727bebaba7bd816af20536e23000c8b155ccbf31
plus <maxcrazy@yeah.net>
2021-04-16 14:48:43 +0800

    release-vcd2cc.kube_proxy.v1_18_1 k8s.gcr.io/kube-proxy:v1.18.1

--- dockerPull.sh
+++ dockerPull.sh
@@ -1,2 +1,3 @@
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:3fa66.kube_apiserver.v1_18_1
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:3d9c7.kube_controller_manager.v1_18_1
+docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:96880.kube_scheduler.v1_18_1
--- dockerTag.sh
+++ dockerTag.sh
@@ -1,2 +1,3 @@
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:3fa66.kube_apiserver.v1_18_1 k8s.gcr.io/kube-apiserver:v1.18.1
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:3d9c7.kube_controller_manager.v1_18_1 k8s.gcr.io/kube-controller-manager:v1.18.1
+docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:96880.kube_scheduler.v1_18_1 k8s.gcr.io/kube-scheduler:v1.18.1
