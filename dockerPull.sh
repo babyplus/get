37bca5715eded2b5ce45790dc5122fe8e48e9f68
plus <maxcrazy@yeah.net>
2021-04-16 14:48:03 +0800

    release-v96880.kube_scheduler.v1_18_1 k8s.gcr.io/kube-scheduler:v1.18.1

--- dockerPull.sh
+++ dockerPull.sh
@@ -1,1 +1,2 @@
 docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:3fa66.kube_apiserver.v1_18_1
+docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:3d9c7.kube_controller_manager.v1_18_1
--- dockerTag.sh
+++ dockerTag.sh
@@ -1,1 +1,2 @@
 docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:3fa66.kube_apiserver.v1_18_1 k8s.gcr.io/kube-apiserver:v1.18.1
+docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:3d9c7.kube_controller_manager.v1_18_1 k8s.gcr.io/kube-controller-manager:v1.18.1
