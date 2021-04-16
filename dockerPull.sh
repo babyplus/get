70fe26db834b2935392e32c8e6b45a06255371da
plus <maxcrazy@yeah.net>
2021-04-16 11:09:43 +0800

    release-vddfb2.kube_apiserver.v1_18_1 k8s.gcr.io/kube-apiserver:v1.18.1

--- dockerPull.sh
+++
@@ -1,6 +0,0 @@
-docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:aa696.kube_apiserver.v1_15_1
-docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:c3c03.kube_controller_manager.v1_15_1
-docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:02ec0.kube_scheduler.v1_15_1
-docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:8dad4.kube_proxy.v1_15_1
-docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:84041.pause.3_1
-docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:72e7e.etcd.3_3_10
