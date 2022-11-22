a56de28bf1c84ab3123ad4430dfbb2587f063145
root <root@localhost.localdomain>
2022-11-22 11:35:58 +0800

    release-va221122cb163.redis.6_2_7 redis:6.2.7

---
+++ dockerPull.sh
@@ -0,0 +1,1 @@
+docker pull registry.cn-hangzhou.aliyuncs.com/babyplus/get:a221122c5dec.redis.7_0_5
---
+++ dockerTag.sh
@@ -0,0 +1,1 @@
+docker tag registry.cn-hangzhou.aliyuncs.com/babyplus/get:a221122c5dec.redis.7_0_5 redis:7.0.5
