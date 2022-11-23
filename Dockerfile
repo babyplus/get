24ee7aae468f3764bf62a5c83f5bcddb0e8b6553
root <root@localhost.localdomain>
2022-11-23 10:37:52 +0800

    release-vb22112324bc6.my_ansible.0_0_1 my_ansible:0.0.1

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM registry.cn-hangzhou.aliyuncs.com/babyplus/get:a221122c4e6a.ubuntu.20_04
+FROM registry-internal.cn-hangzhou.aliyuncs.com/babyplus/get:a221122c4e6a.ubuntu.20_04
 CMD ["bash"]
 RUN /bin/sh -c set -ex; apt-get update; apt-get install ansible sshpass -y
--- build.sh
+++ build.sh
@@ -8,5 +8,17 @@ string=""
 > dockerTag.sh
 
 build $*
-mkdir -p records
-cat dockerPull.sh dockerTag.sh | sort -k 3 > records/`date +%s`
+for n in `seq 0 6`
+do
+echo +++++++1
+    bash dockerPull.sh && {
+echo +++++++2
+        mkdir -p records
+        cat dockerPull.sh dockerTag.sh | sort -k 3 > records/`date +%s`
+        exit 0
+    }
+    sleep 180
+echo ++++==3
+done
+
+exit 1
