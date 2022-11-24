039154f1c8c72885e946b032ffb269e6fef79bf1
root <root@localhost.localdomain>
2022-11-24 21:55:42 +0800

    release-va221124af391.geojson_io.latest kaeff/geojson.io:latest

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,1 @@
-FROM registry.cn-hangzhou.aliyuncs.com/babyplus/get:02490.node.lts-slim
-RUN npm install -g coordtransform-cli
-# Start
-CMD coordtransform
+FROM kaeff/geojson.io:latest
--- build.sh
+++ build.sh
@@ -10,15 +10,12 @@ string=""
 build $*
 for n in `seq 0 6`
 do
-echo +++++++1
     bash dockerPull.sh && {
-echo +++++++2
         mkdir -p records
         cat dockerPull.sh dockerTag.sh | sort -k 3 > records/`date +%s`
         exit 0
     }
     sleep 180
-echo ++++==3
 done
 
 exit 1
